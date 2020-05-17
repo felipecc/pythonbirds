2f3bb882762a91d09c1a1070551b9ce50b58946d
Felipe Caetano <caetano.felipe@gmail.com>
2020-05-17 11:11:48 -0300

    Adicionados atributos do objetos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,15 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+if __name__ == '__main__':
+    p = Pessoa('Maria')
+
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
